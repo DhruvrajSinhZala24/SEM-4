cf65502daed9239d1fa7635be9ad8a60b7a82e99
DhruvrajSinh Zala <157804365+DhruvrajSinhZala24@users.noreply.github.com>
2025-05-29 11:43:09 +0530

    Add files via upload

---
+++ ZALA DHRUVRAJSINH/FSD 2/T3/Unit 6/src/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+import Car from './car'
+function App(){
+  // const name="HTML"1
+  // const mystyle={color:"green",fontfamily:"Times new roman"}5
+  return(
+    // <>
+    // {/* <p style={mystyle}>How are yhou brother</p>5 */}
+    // </>
+    // <h1>React is {name} times better with jsx</h1>1
+    // <h1 style={{color:'red'}}>Hello World</h1>2
+    // <h1 className="c1">Hello World</h1>3
+    // for multiple lines:-4
+    // <>
+    // {/* <h1 className="c1">Hello World</h1>
+    // <p className="c1">How Are You</p> */}
+    // {/*this isfor multiline comment
+    // */}
+    // {/* <img src='img.png' alt='not loaded' />  */}
+    // {/*This image is in public folder but if it is in src folder then need to:- import img from './img.png' and src={img}
+    // */} 
+    // </>
+    <Car/>
+    // refer from notes 
+  );
+}
+export default App
---
+++ ZALA DHRUVRAJSINH/FSD 2/T3/Unit 6/src/car.js
@@ -0,0 +1,4 @@
+function car(){
+    return(<h2>Hi, I am a car</h2>)
+}
+export default car
