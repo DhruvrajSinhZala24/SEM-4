573077ec5cb0b9de7f2b3b886fd91c22aa48f8a2
DhruvrajSinh Zala <157804365+DhruvrajSinhZala24@users.noreply.github.com>
2025-05-29 12:01:50 +0530

    Add files via upload

---
+++ ZALA DHRUVRAJSINH/FSD 2/T3/Unit 6/React Props 2/src/App.js
@@ -0,0 +1,11 @@
+import Example from "./Example";
+function App(){
+  return(
+   <>
+    <Example name='XYZ' price='70000'/>
+    <Example name='ABC' price='40000'/>
+    </>
+
+  );
+}
+export default App
---
+++ ZALA DHRUVRAJSINH/FSD 2/T3/Unit 6/React Props 2/src/Example.js
@@ -0,0 +1,7 @@
+function Example(props){
+    return(
+        <>
+        <h1>{props.name}:{props.price}</h1>
+        </>
+    )
+}export default Example
