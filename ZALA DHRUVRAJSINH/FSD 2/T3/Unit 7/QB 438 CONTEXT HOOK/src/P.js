a710b86e82448f47681036ebc616361c368a8d90
DhruvrajSinh Zala <157804365+DhruvrajSinhZala24@users.noreply.github.com>
2025-06-10 09:46:05 +0530

    Update P.js

--- ZALA DHRUVRAJSINH/FSD 2/T3/Unit 7/QB 438 CONTEXT HOOK/src/P.js
+++ ZALA DHRUVRAJSINH/FSD 2/T3/Unit 7/QB 438 CONTEXT HOOK/src/P.js
@@ -1,3 +1,12 @@
+//Create a React app with three components: parent component P.js and two
+// child components C1.js, C2.js.
+//  Use useContext hook to pass two colors from the Parent component P.js to the
+// Child component C2.js. (Yellow color as font color and Blue color as
+// background color)
+// In Child component C1.js import child component C2.js .
+// In C2.js display “Full stack Development” text in h1 heading with above
+// mentioned background color and font color using context.
+// QB 437 is same
 import { createContext} from "react";
 import C1 from "./c1";
 const col1=createContext()
